e1e0154262637152c2eeda9b7a7d5d58fb5b4a21
Imants Ostass <imantso@gmail.com>
2016-11-01 13:23:41 +0200

    Remade according to tutorial

--- app/src/main/java/a1stgroup/gpsalarm/MapsActivity.java
+++ app/src/main/java/a1stgroup/gpsalarm/MapsActivity.java
@@ -1,61 +1,114 @@
 package a1stgroup.gpsalarm;
 
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+        import android.app.Dialog;
+        import android.location.Address;
+        import android.location.Geocoder;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+        import com.google.android.gms.common.ConnectionResult;
+        import com.google.android.gms.common.GoogleApiAvailability;
+        import com.google.android.gms.maps.CameraUpdate;
+        import com.google.android.gms.maps.CameraUpdateFactory;
+        import com.google.android.gms.maps.GoogleMap;
+        import com.google.android.gms.maps.MapFragment;
+        import com.google.android.gms.maps.OnMapReadyCallback;
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+        import java.io.IOException;
+        import java.util.List;
 
-    private GoogleMap mMap;
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap myGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "Good: Google Services Available!", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_maps);
+            initMap();
+        } else {
+            // No Google Maps Supported layout.
+        }
+
+
+    }
+
+    private void initMap() {
+
+        MapFragment myMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+
+        myMapFragment.getMapAsync(this);            // Previously getMap
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    public boolean googleServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);      // Can return 3 different values
+
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't connect to play services!", Toast.LENGTH_LONG).show();
+        }
+
+        return false;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        myGoogleMap = googleMap;
+        goToLocation(57.062133, 24.025366);
+        zoom(15);
 
-        // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        myGoogleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        myGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-
-    // This method can be used to go to a specific latitude/longitude.
     private void goToLocation(double lat, double lng) {
         LatLng coordinates = new LatLng(lat, lng);
         CameraUpdate camUpdate = CameraUpdateFactory.newLatLng(coordinates);
-        mMap.moveCamera(camUpdate);
+        myGoogleMap.moveCamera(camUpdate);
     }
 
-    // This method can be used to zoom map to a particular zoom level.
+
     private void zoom(float zoom) {
         CameraUpdate cameraUpdate = CameraUpdateFactory.zoomTo(zoom);
-        mMap.moveCamera(cameraUpdate);
+        myGoogleMap.moveCamera(cameraUpdate);
     }
+
+    /*public void geoLocate(View view) throws IOException {
+        EditText et = (EditText) findViewById(R.id.editText);
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(this);   // Takes any string and converts to latitude / longitude.
+
+        List<Address> list = gc.getFromLocationName(location, 1);
+        Address address = list.get(0);      // This object is filled with lots of information.
+        String locality = address.getLocality();                  // Locality here just for demonstartion purposes.
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+        goToLocation(lat, lng);
+        zoom(15);
+
+    }*/
+
 }
